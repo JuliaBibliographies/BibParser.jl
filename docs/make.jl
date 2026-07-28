ddddd6fc6d5c586b97995bbb7cda3610b35df62b
azzaare <jf@baffier.fr>
2026-07-28 11:20:44 +0200

    Fix docs and format

--- docs/make.jl
+++ docs/make.jl
@@ -1,7 +1,4 @@
-import Pkg
-Pkg.develop(path = joinpath(@__DIR__, "..", "..", "BibInternal"))
-Pkg.develop(path = joinpath(@__DIR__, ".."))
-Pkg.instantiate()
+
 using Documenter, BibParser, BibInternal
 
 makedocs(
